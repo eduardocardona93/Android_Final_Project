f84bc8a87204944d26f2c3442b4472bf5d471f1b
Ranjana <ranjana.madt@gmail.com>
2021-04-16 13:01:43 +0530

    Font,Style,Theme

--- app/src/main/java/com/example/android_final_project/LoginActivity.java
+++ app/src/main/java/com/example/android_final_project/LoginActivity.java
@@ -2,13 +2,17 @@ package com.example.android_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     EditText loginUserFld, logInPasswordField;
-    Button loginBtn, registerBtn;
+    Button loginBtn;
+    TextView registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,24 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         registerBtn = findViewById(R.id.registerBtn);
 
+        loginBtn.setOnClickListener(this);
+        registerBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.loginBtn :{
+                startActivity(new Intent(this,HomeActivity.class));
+                break;
+            }
+            case R.id.registerBtn : {
+                startActivity(new Intent(this,RegisterActivity.class));
+                break;
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
--- app/src/main/java/com/example/android_final_project/RegisterActivity.java
+++ app/src/main/java/com/example/android_final_project/RegisterActivity.java
@@ -3,12 +3,15 @@ package com.example.android_final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     EditText userNameFld, userFullnameFld, userPhoneFld, userEmailFld, userPasswordFld,userRetypePassFld;
     Button registerUserButton;
+    ImageView imgBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,23 @@ public class RegisterActivity extends AppCompatActivity {
         userPasswordFld = findViewById(R.id.userPasswordFld);
         userRetypePassFld = findViewById(R.id.userRetypePassFld);
         registerUserButton = findViewById(R.id.registerUserButton);
+        imgBack=findViewById(R.id.imgBack);
 
+        imgBack.setOnClickListener(this);
+        registerUserButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgBack:{
+                finish();
+                break;
+            }
+            case R.id.registerUserButton:{
+                break;
+            }
+        }
     }
 }
